ff0a71b14a8f33a9375e13553ecbb7f90efed674
RomaSamkov <Romansamkov.rs@gmail.com>
2025-01-21 16:54:50 +0200

    add getMovieDetails

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -29,3 +29,29 @@ export async function getMovieTrailers(req, res) {
     res.status(500).json({ success: false, message: "Internal server error." });
   }
 }
+
+export async function getMovieDetails(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}?language=en-US`
+    );
+    res.json({ success: true, content: data });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+    console.log("Error in details controller", error.message);
+    res.status(500).json({ success: false, message: "Internal server error." });
+  }
+}
+
+export async function getSimilarMovies(req, res) {
+  // https://api.themoviedb.org/3/movie/movie_id/similar?language=en-US&page=1
+  const { id } = req.params;
+  try {
+  } catch (error) {
+    console.log("Error in similar controller", error.message);
+    res.status(500).json({ success: false, message: "Internal server error." });
+  }
+}
