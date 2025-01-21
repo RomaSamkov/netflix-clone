b2a0f05ec80fdb0fff681b690ec7175aede9386d
RomaSamkov <Romansamkov.rs@gmail.com>
2025-01-21 16:54:39 +0200

    add getMovieDetails

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,6 +1,8 @@
 import express from "express";
 import {
+  getMovieDetails,
   getMovieTrailers,
+  getSimilarMovies,
   getTrendingMovie,
 } from "../controllers/movie.controller.js";
 
@@ -8,5 +10,7 @@ const router = express.Router();
 
 router.get("/trending", getTrendingMovie);
 router.get("/:id/trailers", getMovieTrailers);
+router.get("/:id/details", getMovieDetails);
+// router.get("/:id/similar", getSimilarMovies);
 
 export default router;
