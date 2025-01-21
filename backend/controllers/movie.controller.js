28cc862cf8e979ecace09fd6cfc5e32920ebf7a0
RomaSamkov <Romansamkov.rs@gmail.com>
2025-01-21 17:21:45 +0200

    add getMoviesByCategory

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -7,7 +7,7 @@ export async function getTrendingMovie(req, res) {
     );
     const randomMovie =
       data.results[Math.floor(Math.random() * data.results?.length)];
-    res.json({ success: true, content: randomMovie });
+    res.status(200).json({ success: true, content: randomMovie });
   } catch (error) {
     console.log("Error in movie controller", error.message);
     res.status(500).json({ success: false, message: "Internal server error." });
@@ -20,7 +20,7 @@ export async function getMovieTrailers(req, res) {
     const data = await fetchFromTMDB(
       `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`
     );
-    res.json({ success: true, trailers: data.results });
+    res.status(200).json({ success: true, trailers: data.results });
   } catch (error) {
     if (error.message.includes("404")) {
       return res.status(404).send(null);
@@ -36,7 +36,7 @@ export async function getMovieDetails(req, res) {
     const data = await fetchFromTMDB(
       `https://api.themoviedb.org/3/movie/${id}?language=en-US`
     );
-    res.json({ success: true, content: data });
+    res.status(200).json({ success: true, content: data });
   } catch (error) {
     if (error.message.includes("404")) {
       return res.status(404).send(null);
@@ -52,9 +52,22 @@ export async function getSimilarMovies(req, res) {
     const data = await fetchFromTMDB(
       `https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`
     );
-    res.json({ success: true, content: data.results });
+    res.status(200).json({ success: true, content: data.results });
   } catch (error) {
     console.log("Error in similar controller", error.message);
+    res.status(500).json({ success: false, message: "Internal server error." });
+  }
+}
+
+export async function getMoviesByCategory(req, res) {
+  const { category } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`
+    );
+    res.status(200).json({ success: true, content: data.results });
+  } catch (error) {
+    console.log("Error in getMoviesByCategory controller", error.message);
     res.status(500).json({ success: false, message: "Internal server error." });
   }
 }
