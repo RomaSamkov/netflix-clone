d0a656ec886ffb0fa7cd708b968a502dbea56199
RomaSamkov <Romansamkov.rs@gmail.com>
2025-01-27 17:42:58 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-violet-800 text-center">
+      <h1 className="text-3xl font-bold text-violet-800 text-center">
         Hello world!
       </h1>
     </>
