b0a5e61d8539940cf6b4ddbc5adeb8fbc26309ee
RomaSamkov <Romansamkov.rs@gmail.com>
2025-01-28 20:13:04 +0200

    Update SignUpPage.jsx

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  return (
+    <div className="hero-bg h-screen w-full">
+      <header className="mx-auto max-w-6xl flex items-center justify-between p-4">
+        <Link to={"/"}>
+          <img src="/netflix-logo.png" alt="logo" className="w-52" />
+        </Link>
+      </header>
+      <div className="flex justify-center items-center mt-20 mx-3">
+        <div className="w-full max-w-md"></div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
