5935c01f012a28c187bf06c5fa5bfe955aac2e84
RomaSamkov <Romansamkov.rs@gmail.com>
2025-01-31 19:12:27 +0200

    Update HomePage.jsx

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,9 +1,10 @@
+import { useAuthStore } from "../../store/AuthUser";
 import AuthScreen from "./AuthScreen";
 import HomeScreen from "./HomeScreen";
 
 const HomePage = () => {
-  const user = false;
-  return <div>{user ? <HomeScreen /> : <AuthScreen />}</div>;
+  const { user } = useAuthStore();
+  return <>{user ? <HomeScreen /> : <AuthScreen />}</>;
 };
 
 export default HomePage;
