1f8d3c8df129204cd054deda99762d08d2da83ce
RomaSamkov <Romansamkov.rs@gmail.com>
2025-02-06 17:58:28 +0200

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,8 +6,8 @@ export const generateTokenAndSetCookie = (userId, res) => {
   res.cookie("jwt-netflix", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    // sameSite: "strict",
-    // secure: ENV_VARS.NODE_ENV !== "development",
+    sameSite: "strict",
+    secure: ENV_VARS.NODE_ENV !== "development",
   });
   return token;
 };
